cf95505a6aeee5bcfaac76f8565c36f21693186b
AziTimur <azik00kgz@gmail.com>
2021-05-25 22:44:19 +0600

    Initial commit

---
+++ app/src/main/java/com/geek/homework26/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.geek.homework26;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout2);
+    }
+}
